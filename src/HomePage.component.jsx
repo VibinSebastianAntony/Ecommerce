21ab12fdeff08878642de3873c8ab2f60f14fc0f
VibinSebastianAntony <vibinroxx@gmail.com>
2021-04-15 18:32:23 +0530

    created using create-react-app and created homepage layout

---
+++ src/HomePage.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './HomePage.styles.scss'
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HAT'S</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHOES</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHIRTS</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHIRTS</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>SHIRTS</h1>
+                        <span className='subtitle'>Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
